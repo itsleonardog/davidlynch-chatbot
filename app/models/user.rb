7120ec2b7fbcfb631d8494950796a0483b5f6d75
Leonardo Genzolini <leonardogenzolini@gmail.com>
2025-01-08 15:57:33 +0100

    moving it all to the model. bugs to fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def content
+    client = OpenAI::Client.new
+    chatgpt_response = client.chat(parameters: {
+      model: "gpt-4o-mini",
+      messages: [{ role: "user", content: "Who is David Lynch, and how is he related to Twin Peaks?" }]
+    })
+    chatgpt_response["choices"][0]["message"]["content"]
+  end
 end
