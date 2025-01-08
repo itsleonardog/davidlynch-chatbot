c2fbb414c9beb4a77e71edbf42b5d914461a233e
Leonardo Genzolini <leonardogenzolini@gmail.com>
2025-01-08 15:35:40 +0100

    openai init and working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    client = OpenAI::Client.new
+    chatgpt_response = client.chat(parameters: {
+      model: "gpt-4o-mini",
+      messages: [{ role: "user", content: "Who is David Lynch, and how is he related to Twin Peaks?" }]
+    })
+    @content = chatgpt_response["choices"][0]["message"]["content"]
   end
 end
