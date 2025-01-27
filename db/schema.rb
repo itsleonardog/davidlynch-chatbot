83ab17308a259739a4522639b54f80575a4f5fc2
Leonardo Genzolini <leonardogenzolini@gmail.com>
2025-01-27 15:45:45 +0100

    turbo stream setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_08_133602) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_27_143110) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "solid_cable_messages", force: :cascade do |t|
+    t.text "channel"
+    t.text "payload"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
+    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
