6683c6b164f8cc0f2758783b635cc206ecf0a200
Leonardo Genzolini <leonardogenzolini@gmail.com>
2025-01-29 17:07:06 +0100

    bare minimum done

--- app/jobs/chatbot_job.rb
+++ app/jobs/chatbot_job.rb
@@ -2,6 +2,13 @@ class ChatbotJob < ApplicationJob
   queue_as :default
 
   def perform(question)
-    # Do something later
+    @question = question
+    chatgpt_response = client.chat(
+      parameters: {
+        model: "gpt-4o-mini",
+        messages: questions_formatted_for_openai # to code as private method
+      }
+    )
+    new_content = chatgpt_response["choices"][0]["message"]["content"]
   end
 end
