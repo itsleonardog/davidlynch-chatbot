904bdd5eae018170b13d0a4454d22d2c5c1fdf98
Leonardo Genzolini <leonardogenzolini@gmail.com>
2025-01-31 16:02:57 +0100

    homepage and why login page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :why_login ]
 
   def home
+  end
+
+  def why_login
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :questions, only: [:index, :create]
+  get "why-login", to: "pages#why_login"
 end
